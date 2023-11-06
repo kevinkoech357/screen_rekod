270e1f30085c0ed19adb428f688abc9829482fbf
Kevin_Koech <kevinkoech@proton.me>
2023-11-06 20:52:52 +0300

    add create db function

---
+++ screen_rekod/utils.py
@@ -0,0 +1,10 @@
+from screen_rekod.extensions import db
+
+def create_database(app):
+    """
+    Create Database
+    """
+
+    with app.app_context():
+        db.create_all()
+        print('Database created successfully')
