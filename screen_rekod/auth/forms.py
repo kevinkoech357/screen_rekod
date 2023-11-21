39cd4ab91d044afd36007780996a4f45d5b32878
Kevin_Koech <kevinkoech@proton.me>
2023-11-21 23:09:50 +0300

    allow user to login using email

--- screen_rekod/auth/forms.py
+++ screen_rekod/auth/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username_or_email = StringField('Username or Email', validators=[validators.DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
