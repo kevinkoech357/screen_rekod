f409375b6141902198de0f39474a017e8b44f78c
Kevin_Koech <kevinkoech@proton.me>
2023-11-27 22:12:14 +0300

    add reset password form

--- screen_rekod/auth/forms.py
+++ screen_rekod/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms import StringField, PasswordField, SubmitField, validators, TextAreaField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
@@ -13,3 +13,10 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+class ResetPasswordForm(FlaskForm):
+    email = StringField('Email', validators=[validators.DataRequired(), validators.Email()])
+    new_password = PasswordField('New Password', validators=[validators.DataRequired()])
+    confirm_new_password = PasswordField('Confirm New Password', validators=[validators.DataRequired(), validators.EqualTo('new_password')])
+    submit = SubmitField('Reset Password')
+
