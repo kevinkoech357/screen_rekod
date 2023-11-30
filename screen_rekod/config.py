84b1e49000b729b633dcf9159fa4c1b287886497
Kevin_Koech <kevinkoech@proton.me>
2023-11-30 14:46:44 +0300

    configure uploads folder and flash message duration

--- screen_rekod/config.py
+++ screen_rekod/config.py
@@ -6,18 +6,17 @@ load_dotenv(".env")
 
 class App_Config:
     # Secret key for signing cookies
-    SECRET_KEY = os.environ.get("SECRET_KEY", "test")
-
+    SECRET_KEY = os.environ.get("SECRET_KEY", "hello_world!")
     # Database URI. Default is SQLite in-memory database
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "SQLALCHEMY_DATABASE_URI", "sqlite:///test.db"
     )
-
     # Disable modification tracking for SQLAlchemy, unless explicitly set to True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     # Disable echoing SQL statements to the console
     SQLALCHEMY_ECHO = False
-
     # Flashed message duration
-    MESSAGE_FLASHING_OPTIONS = {'duration': 2}
+    MESSAGE_FLASHING_OPTIONS = {"duration": 5}
+    SESSION_PERMANENT = False
+    # Video uploads folder
+    UPLOAD_FOLDER = "uploads"
