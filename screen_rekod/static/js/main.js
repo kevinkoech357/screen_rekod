d318f899df27ea366ff1061c4a1bcf14da43b4b5
Kevin_Koech <kevinkoech@proton.me>
2023-11-30 15:14:13 +0300

    use mediarecorder api to enable screen and audio recording

---
+++ screen_rekod/static/js/main.js
@@ -0,0 +1,102 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const startButton = document.getElementById('startRecording');
+  const stopButton = document.getElementById('stopRecording');
+  const recordedVideoModal = document.getElementById('recordedVideoModal');
+  const titleModal = document.getElementById('titleModal');
+  const descriptionModal = document.getElementById('descriptionModal');
+  const uploadButtonModal = document.getElementById('uploadButtonModal');
+
+  if (!startButton || !stopButton || !recordedVideoModal || !titleModal || !descriptionModal || !uploadButtonModal) {
+    console.error('Required elements not found.');
+    return;
+  }
+
+  let chunks = [];
+  let recorder;
+  let recordedBlob;
+
+  startButton.addEventListener('click', function () {
+    if (!navigator.mediaDevices) {
+      console.error('getUserMedia not supported.');
+      return;
+    }
+
+    const constraints = { video: true, audio: true };
+    navigator.mediaDevices.getDisplayMedia(constraints)
+      .then(function (stream) {
+        const mime = MediaRecorder.isTypeSupported('video/webm; codecs=h264')
+          ? 'video/webm; codecs=h264'
+          : 'video/webm';
+
+        recorder = new MediaRecorder(stream, {
+          mimeType: mime
+        });
+
+        chunks = [];
+        recorder.addEventListener('dataavailable', function (e) {
+          chunks.push(e.data);
+        });
+
+        recorder.addEventListener('stop', function () {
+          console.log('Recording stopped.');
+
+          recordedBlob = new Blob(chunks, { type: 'video/webm' });
+          recordedVideoModal.src = URL.createObjectURL(recordedBlob);
+
+          // Open the Bootstrap Modal
+          $('#videoModal').modal('show');
+        });
+
+        recorder.onstart = event => {
+          console.log('Recording started.');
+          startButton.disabled = true;
+        };
+
+        recorder.start();
+      })
+      .catch(function (err) {
+        console.error('getUserMedia error:', err);
+      });
+  });
+
+  stopButton.onclick = () => {
+    if (recorder && recorder.state === 'recording') {
+      recorder.stop();
+      console.log('Recorder stopped manually.');
+    }
+  };
+
+  uploadButtonModal.addEventListener('click', function () {
+    if (!recordedBlob) {
+      console.error('No recorded video to upload.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('video_file', recordedBlob);
+    formData.append('title', titleModal.value);
+    formData.append('description', descriptionModal.value);
+
+    console.log('Uploading...');
+    console.log('Title:', titleModal.value);
+    console.log('Description:', descriptionModal.value);
+    console.log('Recorded Blob:', recordedBlob);
+
+    fetch('/upload', {
+      method: 'POST',
+      body: formData
+    }).then(resp => {
+      console.log('Upload response:', resp);
+      if (resp.status === 200) {
+        alert('File successfully uploaded!');
+        window.location.reload(true);
+        $('#videoModal').modal('hide');
+      } else {
+        alert('Error uploading File!');
+        console.error('Error:', resp);
+      }
+    }).catch(err => {
+      console.error('Upload error:', err);
+    });
+  });
+});
