5df6ea49615e42934798e305822c49db3ecad097
Kevin_Koech <kevinkoech@proton.me>
2023-11-30 15:07:25 +0300

    add reset_password route that will enable user to reset password based on email

--- screen_rekod/auth/routes.py
+++ screen_rekod/auth/routes.py
@@ -4,91 +4,128 @@ from screen_rekod.auth.forms import LoginForm, RegistrationForm, ResetPasswordFo
 from screen_rekod.models.user import User
 from screen_rekod import db
 
-auth = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
+auth = Blueprint("auth", __name__, template_folder="templates", static_folder="static")
 
-@auth.route('/login', methods=['GET', 'POST'])
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    Log in a user.
+
+    If the user is already authenticated, redirects to the dashboard.
+    If the form is submitted and valid, logs in the user and redirects to the dashboard.
+    """
     if current_user.is_authenticated:
-        return redirect(url_for('user.index'))  # Redirect to home if already logged in
+        return redirect(
+            url_for("user.dashboard")
+        )  # Redirect to dashboard if already logged in
 
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter((User.username == form.username_or_email.data) | (User.email == form.username_or_email.data)).first()
+        user = User.query.filter(
+            (User.username == form.username_or_email.data)
+            | (User.email == form.username_or_email.data)
+        ).first()
         if user and user.check_password(form.password.data):
             login_user(user)
-            flash('Login successful!', 'success')
-            return redirect(url_for('user.index'))
+            flash("Login successful!", "success")
+            return redirect(url_for("user.dashboard"))
         else:
-            flash('Invalid username or password', 'danger')
+            flash("Invalid username or password", "danger")
 
-    return render_template('login.html', form=form)
+    return render_template("login.html", form=form)
 
 
-@auth.route('/register', methods=['GET', 'POST'])
+@auth.route("/register", methods=["GET", "POST"])
 def register():
+    """
+    Register a new user.
+
+    If the user is already authenticated, redirects to the dashboard.
+    If the form is submitted and valid, creates a new user and redirects to the login page.
+    """
     if current_user.is_authenticated:
-        return redirect(url_for('user.index'))  # Redirect to home if already logged in
+        return redirect(
+            url_for("user.dashboard")
+        )  # Redirect to dashboard if already logged in
 
     form = RegistrationForm()
     if form.validate_on_submit():
         existing_email_user = User.query.filter_by(email=form.email.data).first()
-        existing_username_user = User.query.filter_by(username=form.username.data).first()
+        existing_username_user = User.query.filter_by(
+            username=form.username.data
+        ).first()
 
         if existing_email_user:
-            flash('Email already in use. Please choose another email.', 'danger')
-            return render_template('register.html', form=form)
+            flash("Email already in use. Please choose another email.", "danger")
+            return render_template("register.html", form=form)
 
         if existing_username_user:
-            flash('Username already in use. Please choose another username.', 'danger')
-            return render_template('register.html', form=form)
+            flash("Username already in use. Please choose another username.", "danger")
+            return render_template("register.html", form=form)
 
         if form.password.data != form.confirm_password.data:
-            flash('Passwords do not match. Please enter matching passwords.', 'danger')
-            return render_template('register.html', form=form)
+            flash("Passwords do not match. Please enter matching passwords.", "danger")
+            return render_template("register.html", form=form)
 
         # Create a new user instance
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-
+        new_user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
 
         db.session.add(new_user)
         db.session.commit()
 
-        flash('Registration successful! You can now log in.', 'success')
+        flash("Registration successful! You can now log in.", "success")
 
-        return redirect(url_for('auth.login'))
+        return redirect(url_for("auth.login"))
 
-    return render_template('register.html', form=form)
+    return render_template("register.html", form=form)
 
-@auth.route('/logout')
+
+@auth.route("/logout")
 @login_required
 def logout():
+    """
+    Log out the current user.
+
+    Redirects to the home page after logout.
+    """
     logout_user()
-    flash('You have been logged out.', 'success')
-    return redirect(url_for('user.index'))  # Redirect to home after logout
+    flash("You have been logged out.", "success")
+    return redirect(url_for("user.index"))  # Redirect to home after logout
 
 
-@auth.route('/reset-password', methods=['GET', 'POST'])
+@auth.route("/reset-password", methods=["GET", "POST"])
 def reset_password():
+    """
+    Reset user password.
+
+    If the user is already authenticated, redirects to the dashboard.
+    If the form is submitted and valid, resets the user's password and redirects to the login page.
+    """
     if current_user.is_authenticated:
-        return redirect(url_for('user.index'))  # Redirect to home if already logged in
+        return redirect(
+            url_for("user.dashboard")
+        )  # Redirect to dashboard if already logged in
 
     form = ResetPasswordForm()
     if form.validate_on_submit():
         user = User.query.filter(User.email == form.email.data).first()
 
         if not user:
-            flash('Invalid email', 'danger')
-            return render_template('reset_password.html', form=form)
+            flash("Invalid email", "danger")
+            return render_template("reset_password.html", form=form)
 
         if form.new_password.data != form.confirm_new_password.data:
-            flash('Passwords do not match. Please enter matching passwords.', 'danger')
-            return render_template('reset_password.html', form=form)
+            flash("Passwords do not match. Please enter matching passwords.", "danger")
+            return render_template("reset_password.html", form=form)
 
         user.set_password(form.new_password.data)
         db.session.commit()
-        flash('Password reset successful! You can now log in.', 'success')
-        return redirect(url_for('auth.login'))
+        flash("Password reset successful! You can now log in.", "success")
+        return redirect(url_for("auth.login"))
 
-    return render_template('reset_password.html', form=form)
-
-
+    return render_template("reset_password.html", form=form)
