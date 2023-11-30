e59831f5342261237b7c4398ee580b44b49f98eb
Kevin_Koech <kevinkoech@proton.me>
2023-11-30 14:56:52 +0300

    add reset password form

--- screen_rekod/auth/forms.py
+++ screen_rekod/auth/forms.py
@@ -1,22 +1,48 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, validators, TextAreaField, FileField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms import validators, TextAreaField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
+
 class LoginForm(FlaskForm):
-    username_or_email = StringField('Username or Email', validators=[validators.DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    """
+    Form for user login.
+    """
+
+    username_or_email = StringField(
+        "Username or Email", validators=[validators.DataRequired()]
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+    """
+    Form for user registration.
+    """
+
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=3, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Register")
+
 
 class ResetPasswordForm(FlaskForm):
-    email = StringField('Email', validators=[validators.DataRequired(), validators.Email()])
-    new_password = PasswordField('New Password', validators=[validators.DataRequired()])
-    confirm_new_password = PasswordField('Confirm New Password', validators=[validators.DataRequired(), validators.EqualTo('new_password')])
-    submit = SubmitField('Reset Password')
+    """
+    Form for resetting user password.
+    """
 
+    email = StringField(
+        "Email", validators=[validators.DataRequired(), validators.Email()]
+    )
+    new_password = PasswordField("New Password", validators=[validators.DataRequired()])
+    confirm_new_password = PasswordField(
+        "Confirm New Password",
+        validators=[validators.DataRequired(), validators.EqualTo("new_password")],
+    )
+    submit = SubmitField("Reset Password")
