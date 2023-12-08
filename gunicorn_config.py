ae966ae6e1786d7992151dea1cc6198124b19b20
Kevin Koech <kevinkoech@proton.me>
2023-12-08 07:34:43 +0000

    update gunicorn configuration

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,11 @@
 limit_request_line = 0
-limit_request_fields = 32768.0
+limit_request_fields = 32768
 limit_request_field_size = 0
-# limit_request_body = 52428800  # 50 MB in bytes
+limit_request_body = 52428800  # 50 MB in bytes
 
-workers = 4  # Number of worker processes
-bind = "0.0.0.0:8000"  # Host and port to bind to
+bind = '0.0.0.0:8000'
+workers = 3
+loglevel = 'debug'
+accesslog = 'gunicorn_access.log'
+errorlog = 'gunicorn_error.log'
+timeout = 300
