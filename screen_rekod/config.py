15c38edfa2db6076b1ee316c1f1d2a2a86abf070
Kevin Koech <kevinkoech@proton.me>
2024-01-10 14:55:15 +0200

    Update config.py

--- screen_rekod/config.py
+++ screen_rekod/config.py
@@ -18,7 +18,7 @@ class App_Config:
     SQLALCHEMY_ECHO = False
     SESSION_PERMANENT = False
     # Video uploads folder
-    UPLOAD_FOLDER = "/home/ubuntu/screen_rekod/uploads"
+    UPLOAD_FOLDER = "/workspace/uploads"
     # Flask caching Config
     # CACHE_TYPE = "SimpleCache"
     # CACHE_DEFAULT_TIMEOUT = 600
